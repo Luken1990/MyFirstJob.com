d522b2979eff3ad2c1ad9a79a28218bb64217ca9
Luken1990 <L.nguyen_1990@yahoo.com>
2023-06-12 12:06:21 +0100

    Update fetchData.jsx

--- client/src/utilities/fetchData.jsx
+++ client/src/utilities/fetchData.jsx
@@ -2,7 +2,6 @@
 //   method: 'GET',
 //   headers: {
 //     // 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_JSEARCH_KEY,
-//     'X-RapidAPI-Key': 'ecf60d8f74msh5f7e223ed2076cfp13eebdjsn69c45e6de017',
 //     'X-RapidAPI-Host': 'jsearch.p.rapidapi.com',
 //   },
 // };
@@ -25,7 +24,7 @@ import axios from 'axios';
 //   },
 //   headers: {
 //     'content-type': 'application/octet-stream',
-//     // 'X-RapidAPI-Key': '4e3a2722f7msh233ff0c6ba70c89p166c1fjsnef615adb4095',
+//     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_JSEARCH_KEY,
 //     'X-RapidAPI-Host': 'jsearch.p.rapidapi.com',
 //   },
 // };
